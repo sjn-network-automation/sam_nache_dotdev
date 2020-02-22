f4fb93386d015197b1fcd16bfaff0f17ee1fd62f
Sam <no@no.com>
2020-02-22 17:30:42 -0600

    Updates

---
+++ precache-manifest.9d8e06b7591f7b0a1583bce135078939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802df8a9bdd707ce0354dba3d22425f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace1cf377d9fe5d45552",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7708e5a8a86188105796",
+    "url": "/static/js/2.e4e59283.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e4e59283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace1cf377d9fe5d45552",
+    "url": "/static/js/main.c92bd3d5.chunk.js"
+  },
+  {
+    "revision": "4188dfa93483f7d91375",
+    "url": "/static/js/runtime-main.e2590c98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
